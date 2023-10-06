9676eea1c00a2447f8447ba8d609856d3d5eb166
Brayden Hanna <Bhanna0608@gmail.com>
2023-10-06 18:36:17 -0400

    Add files via upload

---
+++ TheSorts/Program.cs
@@ -0,0 +1,340 @@
+﻿/** -----The Sorts-----
+ * bubble, selection, insertion, quick, merge
+ * @braydenHanna
+ * @11/15/22
+ */
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TheSorts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Calibration
+            Console.BufferWidth = 1000;
+            Console.BufferHeight = 1000;
+            Console.WriteLine("Calibrating...");
+            int[] calibrate = randomIntArray(10000);
+            runAllSorts(calibrate, 0, 2, "12345", 1);
+            StreamReader cReader = new StreamReader("words.txt");
+            string[] calibrateWords = new string[1000];
+            for (int i = 0; i < calibrateWords.Length; i++)
+            {
+                calibrateWords[i] = cReader.ReadLine();
+            }
+            cReader.Close();
+            runAllSorts(calibrateWords, 0, 2, "12345", 1);
+            Console.Clear();
+            #endregion Calibration
+
+            char again = 'Y';
+            while (again.Equals('Y')){
+                Console.Clear();
+
+                Console.WriteLine("----Info----\nThis program modularly times and compares sorting algorithms\n");
+
+                Console.WriteLine("----Configuration----");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("(#1)");
+                Console.ResetColor();
+                Console.Write(" Which algorithm(s) you would like to test? (Ex. Quick and Merge | 45 OR 54 OR 4,5 OR 4 5)\n1: Bubble 2: Insertion 3: Selection 4: Quick 5: Merge\nEnter: ");
+                string sortCode = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("(#2)");
+                Console.ResetColor();
+                Console.Write(" What unit you would like to use?\n1: Ticks 2: Nanoseconds 3: Milliseconds 4: Seconds\nEnter: ");
+                double unit = Convert.ToInt32(Console.ReadLine());
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("(#3)");
+                Console.ResetColor();
+                Console.Write(" What would you like to sort? 1: Ints 2: Strings \nEnter: ");
+                string data = Console.ReadLine();
+                int numArrs = 0;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("(#4)");
+                Console.ResetColor();
+                Console.Write(" How many tests do you want to do?: ");
+                numArrs = Convert.ToInt32(Console.ReadLine());
+
+                Console.SetCursorPosition(0, numArrs + 17);
+                int h = 0;
+
+                if (sortCode.ToString().Contains("1"))
+                {
+                    Console.SetCursorPosition(0, numArrs + 18 + h++);
+                    Console.Write("Bubble\n");
+                }
+                if (sortCode.ToString().Contains("2"))
+                {
+                    Console.SetCursorPosition(0, numArrs + 18 + h++);
+                    Console.Write("Insertion\n");
+                }
+                if (sortCode.ToString().Contains("3"))
+                {
+                    Console.SetCursorPosition(0, numArrs + 18 + h++);
+                    Console.Write("Selection\n");
+                }
+                if (sortCode.ToString().Contains("4"))
+                {
+                    Console.SetCursorPosition(0, numArrs + 18 + h++);
+                    Console.Write("Quick\n");
+                }
+                if (sortCode.ToString().Contains("5"))
+                {
+                    Console.SetCursorPosition(0, numArrs + 18 + h++);
+                    Console.Write("Merge\n");
+                }
+
+                if (data == "2")
+                {
+                    for (int i = 1; i <= numArrs; i++)
+                    {
+                        Console.SetCursorPosition(0, i + 14);
+                        Console.Write("# of strings in array #{0} (MAX: 1500000): ", i);
+                        int numWords = Convert.ToInt32(Console.ReadLine());
+                        Console.SetCursorPosition(i * 16, numArrs + 17);
+                        Console.Write(numWords);
+                        string[] arr = new string[numWords];
+                        StreamReader reader = new StreamReader("words.txt");
+                        for (int f = 0; f < numWords; f++)
+                        {
+                            arr[f] = reader.ReadLine();
+                        }
+                        reader.Close();
+                        runAllSorts(arr, i * 16, numArrs + 18, sortCode, unit);
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i <= numArrs; i++)
+                    {
+
+                        Console.SetCursorPosition(0, i + 14);
+                        Console.Write("# of ints in array #{0}: ", i);
+                        int numInts = Convert.ToInt32(Console.ReadLine());
+                        Console.SetCursorPosition(i * 16, numArrs + 17);
+                        Console.Write(numInts);
+                        int[] arr = new int[numInts];
+                        arr = randomIntArray(numInts);
+                        runAllSorts(arr, i * 16, numArrs + 18, sortCode, unit);
+                    }
+                }
+
+                Console.ResetColor();
+                Console.SetCursorPosition(0, numArrs +h+ 19);
+                Console.Write("Would you like to continue sorting (Y/N): ");
+                again = Convert.ToChar(Console.ReadLine().ToUpper());
+            }
+        }
+        static int[] randomIntArray(int num)
+        {
+            Random r = new Random();
+            int[] arr = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = r.Next(999);
+            }
+            return arr;
+        }
+        public static void runAllSorts(int[] arr, int x, int y, string sortCode, double unit)
+        {
+            int[] times = new int[sortCode.Length];
+            int time;
+            int count = 0;
+
+            int sortCount = 0;
+
+            string unitText = "";
+            switch (unit)
+            {
+                case 1:
+                    unitText = " ticks";
+                    break;
+                case 2:
+                    unitText = " ns";
+                    unit = 0.01;
+                    break;
+                case 3:
+                    unitText = " ms";
+                    unit = 10000;
+                    break;
+                case 4:
+                    unitText = " s";
+                    unit = 10000000;
+                    break;
+                default:
+                    unitText = " ticks";
+                    break;
+            }
+
+
+            Sort s = new Sort();
+
+            if (sortCode.ToString().Contains("1"))
+            {
+                Console.SetCursorPosition(x, y);
+                time = s.Bubble(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("2"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.Insertion(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("3"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.Selection(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("4"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.QuickTimed(arr, 0, arr.Length - 1);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("5"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.MergeTimed(arr, 0, arr.Length - 1);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            for (int k = 0; k < times.Length; k++)
+            {
+                int lowest = int.MaxValue;
+                int lowestY = 0;
+                for (int i = 0; i < times.Length; i++)
+                {
+                    if (times[i] < lowest)
+                    {
+                        lowest = times[i];
+                        lowestY = i + y;
+                    }
+                }
+                Console.BackgroundColor = colorSpectrum(k);
+                Console.SetCursorPosition(x, lowestY);
+                Console.Write(lowest / unit + unitText);
+                Console.ResetColor();
+                times[lowestY - y] = int.MaxValue;
+            }
+        }
+        public static void runAllSorts(string[] arr, int x, int y, string sortCode, double unit)
+        {
+            int[] times = new int[sortCode.Length];
+            int time;
+            int count = 0;
+
+            int sortCount = 0;
+
+            string unitText = "";
+            switch (unit)
+            {
+                case 1:
+                    unitText = " ticks";
+                    break;
+                case 2:
+                    unitText = " ns";
+                    unit = 0.01;
+                    break;
+                case 3:
+                    unitText = " ms";
+                    unit = 10000;
+                    break;
+                case 4:
+                    unitText = " s";
+                    unit = 10000000;
+                    break;
+                default:
+                    unitText = " ticks";
+                    break;
+            }
+
+
+            Sort s = new Sort();
+
+            if (sortCode.ToString().Contains("1"))
+            {
+                Console.SetCursorPosition(x, y);
+                time = s.Bubble(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("2"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.Insertion(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("3"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.Selection(arr);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("4"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.QuickTimed(arr, 0, arr.Length - 1);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            if (sortCode.ToString().Contains("5"))
+            {
+                Console.SetCursorPosition(x, y + sortCount++);
+                time = s.MergeTimed(arr, 0, arr.Length - 1);
+                Console.Write(time / unit + unitText);
+                times[count++] = time;
+            }
+
+            for (int k = 0; k < times.Length; k++)
+            {
+                int lowest = int.MaxValue;
+                int lowestY = 0;
+                for (int i = 0; i < times.Length; i++)
+                {
+                    if (times[i] < lowest)
+                    {
+                        lowest = times[i];
+                        lowestY = i + y;
+                    }
+                }
+                Console.BackgroundColor = colorSpectrum(k);
+                Console.SetCursorPosition(x, lowestY);
+                Console.Write(lowest / unit + unitText);
+                Console.ResetColor();
+                times[lowestY - y] = int.MaxValue;
+            }
+        }
+        public static ConsoleColor colorSpectrum(int i)
+        {
+            if (i == 0) return ConsoleColor.DarkGreen;
+            else if (i == 1) return ConsoleColor.Green;
+            else if (i == 2) return ConsoleColor.DarkYellow;
+            else if (i == 3) return ConsoleColor.Red;
+            else return ConsoleColor.DarkRed;
+        }
+    }
+}
---
+++ TheSorts/Sort.cs
@@ -0,0 +1,390 @@
+﻿/**
+ * contains all the sorts for The Sorts project
+ * @braydenhanna
+ * @11/15/22
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace TheSorts
+{
+    class Sort
+    {
+        Stopwatch sw = new Stopwatch();
+
+        #region INTS
+        public int Bubble(int[] oldArr)
+        {
+            int[] arr = copyArray(oldArr);
+
+            
+            sw.Reset();
+            sw.Start();
+            for (int j = 0; j <= arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(ref arr[i], ref arr[i + 1]);
+                    }
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks  );
+        }
+        public int Insertion(int[] oldArr)
+        {
+            int[] arr = copyArray(oldArr);
+
+            sw.Reset();
+            sw.Start();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    if (arr[k] > arr[i])
+                    {
+                        Swap(ref arr[k], ref arr[i]);
+                    }
+                }
+            }
+
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public int Selection(int[] oldArr)
+        {
+            int[] arr = copyArray(oldArr);
+
+            sw.Reset();
+            sw.Start();
+            for (int k = 0; k < arr.Length; k++)
+            {
+                int lowest = Int32.MaxValue;
+                int posLowest = 0;
+                for (int i = k; i < arr.Length; i++)
+                {
+                    if (arr[i] < lowest)
+                    {
+                        posLowest = i;
+                        lowest = arr[i];
+                    }
+                }
+                Swap(ref arr[k], ref arr[posLowest]);
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public int QuickTimed(int[] oldArr, int left, int right)
+        {
+            int[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            Quick(arr, left, right);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            return (int)(ts.Ticks);
+        }
+        public int[] Quick(int[] arr, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = arr[(left+right)/2];
+            while (i<= j)
+            {
+                while (arr[i] < pivot) i++;
+                while (arr[j] > pivot) j--;
+                if (i <= j)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j) Quick(arr, left, j);
+            if (i < right) Quick(arr, i, right);
+
+            return arr;
+        }
+        public int MergeTimed(int[] oldArr, int left, int right)
+        {
+            int[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            MergeSort(arr, left, right);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            return (int)(ts.Ticks);
+        }
+        private void MergeSort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+                Merge(arr, l, m, r);
+            }
+        }
+        private void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1; 
+            int n2 = r - m;       
+            int[] leftArray = new int[n1];
+            int[] rightArray = new int[n2];
+
+            //Split
+            for (int i = 0; i < n1; i++) leftArray[i] = arr[l + i];
+            for (int i = 0; i < n2; i++) rightArray[i] = arr[m + i + 1];
+
+            int x = 0, y = 0, pos = l;
+            while (x < n1 && y < n2)
+            {
+                if (leftArray[x] < rightArray[y])
+                {
+                    arr[pos] = leftArray[x];
+                    x++;
+                }
+                else
+                {
+                    arr[pos] = rightArray[y];
+                    y++;
+                }
+                pos++;
+            }
+
+            //Merge back together
+            while (x < n1)
+            {
+                arr[pos] = leftArray[x];
+                x++;
+                pos++;
+            }
+
+            while (y < n2)
+            {
+                arr[pos] = rightArray[y];
+                y++;
+                pos++;
+            }
+        }
+        #endregion INTS
+
+        #region STRINGS
+        public int Bubble(string[] oldArr)
+        {
+            string[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            for (int j = 0; j <= arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        Swap(ref arr[i], ref arr[i + 1]);
+                    }
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public int Insertion(string[] oldArr)
+        {
+            string[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    if (arr[k].CompareTo(arr[i]) > 0)
+                    {
+                        Swap(ref arr[k], ref arr[i]);
+                    }
+                }
+            }
+
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public int Selection(string[] oldArr)
+        {
+            string[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            for (int k = 0; k < arr.Length; k++)
+            {
+                String lowest = "zzzzzzzzzzzzzzz";
+                for (int i = k; i < arr.Length; i++)
+                {
+                    if (arr[i].CompareTo(lowest) > 0)
+                    {
+                        lowest = arr[i];
+                    }
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public int QuickTimed(string[] oldArr, int left, int right)
+        {
+            string[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            Quick(arr, left, right);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            return (int)(ts.Ticks);
+        }
+        public void Quick(string[] arr, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            string pivot = arr[(left + right) / 2];
+            while (i <= j)
+            {
+                while (arr[i].CompareTo(pivot) < 0) i++;
+                while (arr[j].CompareTo(pivot) > 0) j--;
+
+                if (i <= j)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j) Quick(arr, left, j);
+            if (i < right) Quick(arr, i, right);
+        }
+        public int MergeTimed(string[] oldArr, int left, int right)
+        {
+            string[] arr = copyArray(oldArr);
+            sw.Reset();
+            sw.Start();
+            MergeSort(arr, left, right);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            return (int)(ts.Ticks);
+        }
+        private string[] MergeSort(string[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+                Merge(arr, l, m, r);
+            }
+            return arr;
+        }
+        private void Merge(string[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            string[] leftArray = new string[n1];
+            string[] rightArray = new string[n2];
+
+            //Split
+            for (int i = 0; i < n1; i++) leftArray[i] = arr[l + i];
+            for (int i = 0; i < n2; i++) rightArray[i] = arr[m + i + 1];
+
+            int x = 0, y = 0, pos = l;
+            while (x < n1 && y < n2)
+            {
+                if (leftArray[x].CompareTo(rightArray[y]) < 0)
+                {
+                    arr[pos] = leftArray[x];
+                    x++;
+                }
+                else
+                {
+                    arr[pos] = rightArray[y];
+                    y++;
+                }
+                pos++;
+            }
+
+            //Merge back together
+            while (x < n1)
+            {
+                arr[pos] = leftArray[x];
+                x++;
+                pos++;
+            }
+
+            while (y < n2)
+            {
+                arr[pos] = rightArray[y];
+                y++;
+                pos++;
+            }
+        }
+        #endregion STRINGS
+
+        #region OTHER STUFF
+        private void Swap(ref int x, ref int y)
+        {
+            int temp1 = y;
+            int temp2 = x;
+            x = temp1;
+            y = temp2;
+        }
+        private void Swap(ref string x, ref string y)
+        {
+            string temp1 = y;
+            string temp2 = x;
+            x = temp1;
+            y = temp2;
+        }
+        public int[] copyArray(int[] arr)
+        {
+            int[] arr2 = new int[arr.Length];
+            for (int e = 0; e < arr.Length; e++)
+            {
+                arr2[e] = arr[e];
+            }
+            return arr2;
+        }
+        private string[] copyArray(string[] arr)
+        {
+            string[] arr2 = new string[arr.Length];
+            for (int e = 0; e < arr.Length; e++)
+            {
+                arr2[e] = arr[e];
+            }
+            return arr2;
+        }
+        public void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        public void printArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        #endregion OTHER STUFF
+
+    }
+}
